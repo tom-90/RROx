34403a6f9a2e92a18e264fe0b18ee13a1cbd368a
Faab007NL <mail@fabiandingemans.nl>
2022-01-27 22:50:45 +0100

    Made a start on a page SearchPopup.tsx

---
+++ packages/web/src/components/SearchPopup.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export function SearchPopup({} : {
+
+}){
+
+    return (<div className="search-div">
+        <div className="search-button-div">
+            <button>Search</button>
+        </div>
+        <div className="search-popup-div">
+            <div className="search-popup">
+                <h1>Search</h1>
+            </div>
+        </div>
+    </div>);
+}
