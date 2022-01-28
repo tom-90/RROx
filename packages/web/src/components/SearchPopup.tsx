4a199dceb2a7915c454c13b07894f4eb55916c10
Faab007NL <mail@fabiandingemans.nl>
2022-01-28 11:34:51 +0100

    Added search items

--- packages/web/src/components/SearchPopup.tsx
+++ packages/web/src/components/SearchPopup.tsx
@@ -1,26 +1,63 @@
 import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { Input, Button } from 'antd';
+import { useMapData } from "../helpers/mapData";
+import {FrameDefinitions} from "@rrox/components";
+import { ControlOutlined, AimOutlined } from '@ant-design/icons';
 
 export function SearchPopup({} : {
 
 }){
+    let { serverKey } = useParams();
+    const navigate = useNavigate();
+
     const [ inputValue, setInputValue] = useState('');
     let searchItems : {
+        ID: number,
         title: string,
         desc: string
     }[] = [];
 
-    searchItems.push({
-        title: 'test1',
-        desc: 'a1'
+    const locate = ( ID: number ) => {
+        navigate( `/${serverKey}`, {
+            state: {
+                locate: {
+                    type: 'Frames',
+                    id  : ID,
+                }
+            }
+        });
+    }
+
+    const { data: mapData, refresh: refreshMapData, loaded: mapDataLoaded, features, actions: actions } = useMapData();
+    mapData.Frames.forEach(frame => {
+        let title = `${frame.Name.toUpperCase()}${frame.Name && Number ? ' - ' : ''}${frame.Number.toUpperCase() || ''}`;
+        if(FrameDefinitions[ frame.Type ].engine){
+            searchItems.push({
+                ID: frame.ID,
+                title: title,
+                desc: 'Locomotive - '+frame.Type.toUpperCase()
+            });
+        }else if(FrameDefinitions[ frame.Type ].tender){
+            searchItems.push({
+                ID: frame.ID,
+                title: title.length != 0 ? title : 'Tender',
+                desc: 'Tender - '+frame.Type.toUpperCase()
+            });
+        }else if(FrameDefinitions[ frame.Type ].freight){
+            searchItems.push({
+                ID: frame.ID,
+                title: title.length != 0 ? title : 'Freight',
+                desc: 'Freight - '+frame.Type.toUpperCase()
+            });
+        }
     });
-    searchItems.push({
-        title: 'test2',
-        desc: 'a2'
-    });
-    searchItems.push({
-        title: 'test3',
-        desc: 'b1'
+    mapData.Players.forEach(player => {
+        searchItems.push({
+            ID: player.ID,
+            title: player.Name,
+            desc: 'Player'
+        });
     });
 
     return (<div className="search-div">
@@ -36,17 +73,22 @@ export function SearchPopup({} : {
                 <div className="search-results">
                     {searchItems.filter((item) => {
                         if (inputValue.length !== 0){
-                            return (item.title.includes(inputValue) || item.desc.includes(inputValue));
+                            return (item.title.toLowerCase().includes(inputValue.toLowerCase()) || item.desc.toLowerCase().includes(inputValue.toLowerCase()));
                         }else{
                             return item;
                         }
-                    }).map(({title, desc}) => <div className="search-result">
+                    }).map(({ID, title, desc}, key) => <div className="search-result" key={'search-item-'+key}>
                         <div className="text">
                             <span className="result-title">{title}</span>
                             <span className="result-desc">{desc}</span>
                         </div>
                         <div className="button">
-                            <Button>Go To</Button>
+                            <Button
+                                title="Locate on the map"
+                                icon={<AimOutlined />}
+                                onClick={() => locate( ID )}
+                                size='large'
+                            />
                         </div>
                     </div>)}
                 </div>
