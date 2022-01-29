349a3632668e333f82b835e11c6e0d48e4632e4b
Faab007NL <mail@fabiandingemans.nl>
2022-01-29 16:31:20 +0100

    Add back layout control routes

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -1,4 +1,8 @@
 import * as React from "react";
+
+import {Route, Routes} from "react-router-dom";
+import {useSettings} from "./helpers/settings";
+
 import {EnterKey} from "./Pages/EnterKeyPage";
 import {MapPage} from "./Pages/MapPage";
 import {PlayerSelect} from "./Pages/PlayerSelect";
@@ -6,8 +10,7 @@ import {MapSettings} from "./Pages/MapSettings";
 import {FrameControlsPage} from "./Pages/FrameControlsPage";
 import {FrameControlPage} from "./Pages/FrameControlPage";
 import {FancyControlPage} from "./Pages/FancyControlPage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {useSettings} from "./helpers/settings";
+import {LayoutControlPage} from "./Pages/LayoutControlPage";
 
 export function App(){
     const [ settings ] = useSettings();
@@ -19,9 +22,10 @@ export function App(){
             <Route path="/:serverKey" element={<MapPage />} />
             <Route path="/:serverKey/players" element={<PlayerSelect />} />
             <Route path="/:serverKey/settings" element={<MapSettings />} />
-            <Route path="/:serverKey/controls" element={<FrameControlsPage />} />
-            <Route path="/:serverKey/controls/:id" element={<FrameControlPage />} />
-            <Route path="/:serverKey/controls/:id/fancy" element={<FancyControlPage />} />
+            <Route path="/:serverKey/controls/layout" element={<LayoutControlPage />} />
+            <Route path="/:serverKey/controls/frames" element={<FrameControlsPage />} />
+            <Route path="/:serverKey/controls/frames/:id" element={<FrameControlPage />} />
+            <Route path="/:serverKey/controls/frames/:id/fancy" element={<FancyControlPage />} />
         </Routes>
     );
 }
